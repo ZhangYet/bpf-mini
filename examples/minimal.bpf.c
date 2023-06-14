1fb5a4c9cb325efccc391e12e24fc6ec441a4931
ye.zhang <ye.zhang@shopee.com>
2023-06-14 23:25:02 +0800

    add softirq demo
    Signed-off-by: ye.zhang <ye.zhang@shopee.com>

--- examples/minimal.bpf.c
+++ examples/minimal.bpf.c
@@ -4,7 +4,7 @@
 #include <bpf/bpf_helpers.h>
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
-
+/*
 int my_pid = 0;
 
 SEC("tp/syscalls/sys_enter_write")
@@ -18,4 +18,12 @@ int handle_tp(void *ctx)
 	bpf_printk("BPF triggered from PID %d.\n", pid);
 
 	return 0;
+}
+*/
+
+SEC("tp/irq/softirq_entry")
+int handle_tp(struct trace_event_raw_softirq *ctx)
+{
+  int pid = bpf_get_current_pid_tgid() >> 32;
+  bpf_printk("softirq triggered from PID: %d, vec: %d\n", pid, ctx->vec);
 }
