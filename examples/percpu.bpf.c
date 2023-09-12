8b7740c40e63caaa022c0f0579db73a0c555e90e
Dantezy <zhangyet@gmail.com>
2023-09-12 16:48:14 +0800

    refactor percpu example
    use __sync_fetch_and_add.
    
    Signed-off-by: Dantezy <zhangyet@gmail.com>

--- examples/percpu.bpf.c
+++ examples/percpu.bpf.c
@@ -13,8 +13,7 @@ struct {
   __uint(max_entries, 64);
 } map1 SEC(".maps");
 
-SEC("kprobe/blk_mq_end_request")
-int BPF_KPROBE(blk_mq_end_request, struct request * rq, blk_status_t error)
+inline int counter(blk_status_t error)
 {
   u64 *valp, init = 1;
   u32 key = (u32)error;
@@ -30,20 +29,15 @@ int BPF_KPROBE(blk_mq_end_request, struct request * rq, blk_status_t error)
   return 0;
 }
 
+SEC("kprobe/blk_mq_end_request")
+int BPF_KPROBE(blk_mq_end_request, struct request * rq, blk_status_t error)
+{
+  return counter(error);
+}
+
 SEC("kprobe/scsi_end_request")
 int BPF_KPROBE(probe_scsi_end_request, struct request *req, blk_status_t error,
 	       unsigned int bytes)
 {
-  u64 *valp, init = 1;
-  u32 key = (u32)error;
-
-  valp = bpf_map_lookup_elem(&map1, &key);
-  if (!valp) {
-    bpf_map_update_elem(&map1, &key, &init, BPF_ANY);
-    return 0;
-  }
-
-  __sync_fetch_and_add(valp, 1);
-
-  return 0;
+  return counter(error);
 }
